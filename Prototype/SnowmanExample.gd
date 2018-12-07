a8a70e10689830faf34e8b8e387e9afcf5c85551
Zach777 <themanofthehour7778@gmail.com>
2018-12-07 12:40:20 -0500

    Snowman Jump

--- Prototype/SnowmanExample.gd
+++ Prototype/SnowmanExample.gd
@@ -7,13 +7,23 @@ extends KinematicBody2D
 
 #These are the constants that will always be
 #affecting us.
+const FLOOR = Vector2( 0,-1 )
 const GRAVITY_ADD = 50
 const GRAVITY_MAX = 300
-const FLOOR = Vector2( 0,-1 )
+const JUMP_STRENGTH = -300
+const JUMP_STAGE_1 = 0.011167 * 5
+const JUMP_STAGE_2 = 0.011167 * 8
+const JUMP_STAGE_3 = 0.011167 * 10
+const JUMP_STAGE_MAX = 3
+
 
 #Determines the velocity
 var velocity : Vector2 = Vector2( 0,0 )
-var jump_held : float = 0 #Use this to determine jump height.
+
+#Use this to determine jump height.
+var jump_held : float = 0
+var jump_stage : int = 1
+var jump_mod : Array = [ 0, -140, -200, -300 ]
 
 #Determines how large the snowman is.
 var size = 1
@@ -28,26 +38,16 @@ func _process(delta):
 	
 	#Calculate velocity's y value.
 	velocity.y = min( velocity.y + GRAVITY_ADD, GRAVITY_MAX )
-	
-	#We are on the floor, start the timer to determine
-	#if we are in the air.
-#	if is_on_floor() :
-#		on_floor = true
-#		air_time = 0
-#	elif air_time >= THREE_FRAME :
-#		on_floor = false
 
+	if jump_held > 0 :
+		jump_held( delta )
 
 	if on_floor() :
-		self.modulate = Color( 0,0,0, 1 )
 		jump_held = 0
 		velocity.y = 0
 		if Input.is_action_just_pressed( "jump" ) :
-			velocity.y =  -900
-			jump_held += 1
-	
-	else:
-		self.modulate = Color( 1,1,1,1 )
+			velocity.y = JUMP_STRENGTH
+			jump_held += delta
 	
 	move_and_slide_with_snap( velocity , Vector2( 0, -1 ), FLOOR )
 
@@ -57,6 +57,27 @@ func change_scale( new_scale : float ):
 	#Eventually we will better handle scaling.
 	$Col.shape.extents = original_col_extents * new_scale
 	$Sprite.scale = original_sprite_scale * new_scale
+
+
+func jump_held( delta ):
+	#Determines how strong the jump should be.
+	if Input.is_action_pressed( "jump" ) :
+		jump_held += delta
+		
+		velocity.y = JUMP_STRENGTH + jump_mod[ jump_stage ]
+		if jump_stage == 2 :
+			pass  
+		
+		if jump_held >= get( "JUMP_STAGE_" + str( jump_stage ) ) :
+			jump_stage += 1
+			
+			if jump_stage > JUMP_STAGE_MAX :
+				jump_held = 0
+				jump_stage = 1
+	
+	else:
+		jump_held = 0
+		jump_stage = 1
 
 
 func on_floor():
