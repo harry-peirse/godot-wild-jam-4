1260159032b1c53eecb0a6bc277e4abf94f1f6ad
Zach777 <themanofthehour7778@gmail.com>
2018-12-09 12:05:36 -0500

    Gave SceneBrowser the new levels

--- globals/SceneBrowser.gd
+++ globals/SceneBrowser.gd
@@ -2,7 +2,10 @@ extends Node
 
 
 var scene_list = {
-	"Test" : "res://scenes/prototype/Test.tscn"
+	"Test" : "res://scenes/prototype/Test.tscn",
+	"ChimneyDescent1" : "res://scenes/levels/ChimneyDescent1.tscn",
+	"ChimneyDescent2" : "res://scenes/levels/ChimneyDescent2.tscn",
+	"LivingRoom" : "res://scenes/levels/LivingRoom.tscn"
 }
 
 
