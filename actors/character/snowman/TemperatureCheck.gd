1ff0c272b01e6a41ba35a6ac223b896eb41c4919
Harry Peirse <peirse@gmail.com>
2018-12-10 18:03:46 +0000

    Remove unnecessary logging

--- actors/character/snowman/TemperatureCheck.gd
+++ actors/character/snowman/TemperatureCheck.gd
@@ -6,19 +6,15 @@ var heat = false
 
 func startFeelingCold():
 	cold = true
-	print("feeling cold")
 	
 func stopFeelingCold():
 	cold = false
-	print("not feeling cold")
 	
 func startFeelingHeat():
 	heat = true
-	print("feeling heat")
 	
 func stopFeelingHeat():
 	heat = false
-	print("not feeling heat")
 	
 func _physics_process(delta):
 	if cold:
