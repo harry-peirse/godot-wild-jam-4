4177c3675de53f53b4a95d1432f202a6c54d4c27
iamYohavier <Jonas@Knohl.de>
2018-12-11 01:43:21 +0900

    Graphic and Dialogue update

--- scenes/levels/Intro/Input E (Intro).gd
+++ scenes/levels/Intro/Input E (Intro).gd
@@ -1,8 +1,23 @@
-extends Sprite
+extends Area2D
+
+var in_range_E
 
 func _ready():
 	set_process(true)
 	
+func _on_Area2D2_area_entered(area):
+	$EButton.visible = true
+	$Press.visible = true
+	in_range_E = true
+
+
+func _on_Area2D2_area_exited(area):
+	$EButton.visible = false
+	$Press.visible = false
+	in_range_E = false
+	
 func _process(delta):
-	if(Input.is_key_pressed(KEY_E)):
+	if (in_range_E)&&(Input.is_key_pressed(KEY_E)):
 		get_node("/root/SceneBrowser").load_scene("Start")
+
+
--- scenes/levels/Intro/SickSanta.gd
+++ scenes/levels/Intro/SickSanta.gd
@@ -1,9 +1,17 @@
 extends Area2D
 
+var is_in_area
 
 func _on_Area2D_area_entered(area):
-	$Dialogue.visible = true
-
-
+	$Dialogue1.visible = true
+	is_in_area = true
+	
 func _on_Area2D_area_exited(area):
-	$Dialogue.visible = false
+	is_in_area = false
+	
+
+func _process(delta):
+	if (is_in_area)&&(Input.is_key_pressed(KEY_E)):
+		$Dialogue1.visible = false
+		$Dialogue2.visible = true
+		
