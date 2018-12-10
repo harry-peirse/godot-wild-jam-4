5f70ed22b2dbb971396e73a6bf208d382975826e
Zach777 <themanofthehour7778@gmail.com>
2018-12-10 10:17:44 -0500

    Added Christmas lights to the health bar.

--- actors/HealthBar/HealthBar.gd
+++ actors/HealthBar/HealthBar.gd
@@ -1,8 +1,29 @@
+tool
 extends TextureProgress
+
+
+var change = 0.016667 * 225
 
 
 var health_object
 
+
+func _process(delta):
+	change -= delta
+	
+	if change >= 0 :
+		return
+	
+	if $Lights1.visible == true :
+		$Lights1.visible = false
+		$Lights2.visible = true
+
+	else:
+		$Lights2.visible = false
+		$Lights1.visible = true
+
+	change = 0.011167 * 225
+	
 
 func update_value( new_value ):
 	self.value = new_value
