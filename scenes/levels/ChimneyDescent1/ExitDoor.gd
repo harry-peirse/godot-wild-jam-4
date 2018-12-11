5f92ca441b0c95c3103bf9bd78eef7b79281d486
iamYohavier <Jonas@Knohl.de>
2018-12-12 02:43:40 +0900

    Audio update

--- scenes/levels/ChimneyDescent1/ExitDoor.gd
+++ scenes/levels/ChimneyDescent1/ExitDoor.gd
@@ -1,9 +1,16 @@
 extends Area2D
 
 var is_in_area
+onready var t = get_node("Timer")
+var already = false
 
 func _process(delta):
-	if (is_in_area)&&(Input.is_key_pressed(KEY_E)):
+	if (is_in_area)&&(Input.is_key_pressed(KEY_E)&&(!already)):
+		already=true
+		$EButtonSFX.play()
+		t.set_wait_time(1)
+		t.start()
+		yield(t, "timeout")
 		get_node("/root/SceneBrowser").load_scene("LivingRoom")
 
 func _on_ExitDoor_area_entered(area):
