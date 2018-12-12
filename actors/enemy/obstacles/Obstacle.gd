64c8d090ead69e24c93d3782ca5ed29a2e8b2bb9
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-12-12 13:56:06 -0800

    merging

--- actors/enemy/obstacles/Obstacle.gd
+++ actors/enemy/obstacles/Obstacle.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
+signal hit_obstacle
+
 const OBJECT_SPEED = 300
 
 var direction = Vector2(-1, 0)
+export var damage_amount = 10
 
 func _process(delta):
 	position += OBJECT_SPEED * direction * delta
 	
 	if position.x < 0:
 		queue_free()
+		
+func _on_RigidBody2D_area_shape_entered(area_id, area, area_shape, self_shape):
+	emit_signal("hit_obstacle", damage_amount)
