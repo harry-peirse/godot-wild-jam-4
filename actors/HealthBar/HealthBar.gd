3d82f02a0e133965b680d83570a01090ec482e7d
Zach777 <themanofthehour7778@gmail.com>
2018-12-13 11:39:51 -0500

    Yummy health bars!

--- actors/HealthBar/HealthBar.gd
+++ actors/HealthBar/HealthBar.gd
@@ -1,14 +1,20 @@
 tool
-extends TextureProgress
+extends Node2D
 
 
-var change = 0.016667 * 225
+const HEALTH_75 = Color( 1,1,1,1 )
+const HEALTH_25 = Color( 0,0.95,0.06,1 )
+const HEALTH_0 = Color( 1,0,0,1 )
+
+
+var change = 0.016667 * 150
 
 
 var health_object
 
 
 func _process(delta):
+	#Change the color of the lights.
 	change -= delta
 	
 	if change >= 0 :
@@ -22,13 +28,24 @@ func _process(delta):
 		$Lights2.visible = false
 		$Lights1.visible = true
 
-	change = 0.011167 * 225
+	change = 0.011167 * 175
 	
 
 func update_value( new_value ):
-	self.value = new_value
+	$Health.value = new_value
+	
+	if new_value > 75 :
+		$Health.modulate = HEALTH_75
+	elif new_value > 25 :
+		$Health.modulate = HEALTH_25
+	elif new_value > 0 :
+		$Health.modulate = HEALTH_0
 
 
 func set_health_object( new_object ):
 	health_object = new_object
 	health_object.connect( "health_changed", self, "update_value" )
+
+
+func is_health_bar():
+	pass
