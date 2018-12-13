7cf6ba3ffd162b6a80f7eddeebf1e9fb46a82bbf
iamYohavier <Jonas@Knohl.de>
2018-12-13 11:10:46 +0900

    SFX Gate

--- scenes/levels/Gate+Lever.gd
+++ scenes/levels/Gate+Lever.gd
@@ -2,21 +2,31 @@ extends Area2D
 
 onready var t = get_node("Timer")
 var in_area = false
+var already = false
 
 func _on_GateLever_area_entered(area):
-	in_area=true
-	$Button.visible = true
+	if(!already):
+		in_area=true
+		$Button.visible = true
 
 
 func _on_GateLever_area_exited(area):
-	in_area=false
-	$Button.visible = true
+	if(!already):
+		in_area=false
+		$Button.visible = true
 	
 func _process(delta):
-	if (in_area)&&(Input.is_action_just_pressed("ConfirmButton")):
-		$Gate.play("Open")
+	if (in_area)&&(Input.is_action_just_pressed("ConfirmButton")&&(!already)):
+		already = true
+		$Button.visible = false
 		$Lever.play("Switch")
-		$Gate/StaticBody2D/CollisionShape2D.disabled =true
+		$Lever/LeverSFX.play()
 		t.set_wait_time(1)
 		t.start()
 		yield(t, "timeout")
+		$Gate.play("Open")
+		$Gate/GateSFX.play()
+		t.set_wait_time(1.5)
+		t.start()
+		yield(t, "timeout")
+		$Gate/StaticBody2D/CollisionShape2D.disabled =true
