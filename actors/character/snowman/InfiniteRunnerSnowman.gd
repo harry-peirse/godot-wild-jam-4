48aa6cb7cab46e1d6cbdeb5b277343abcc408ac0
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-12-12 19:48:16 -0800

    subclassing snowman

---
+++ actors/character/snowman/InfiniteRunnerSnowman.gd
@@ -0,0 +1,14 @@
+extends "res://actors/character/snowman/SnowmanInterface.gd"
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("child process")
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
