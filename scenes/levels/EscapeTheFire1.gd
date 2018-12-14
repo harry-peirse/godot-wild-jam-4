01ed1bfc4de525f817fa9e8097466baaf8ad5368
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-12-14 10:27:33 -0800

    fixed the issue with broken references

--- scenes/levels/EscapeTheFire1.gd
+++ scenes/levels/EscapeTheFire1.gd
@@ -10,13 +10,13 @@ export(float) var level_duration = 45 setget _set_level_duration
 func _ready():
 	_start_level()
 	Music.play_track(Music.tracks.panic)
-	$ParallaxBackground.set_ignore_camera_zoom(true)
+	$Background/ParallaxBackground.set_ignore_camera_zoom(true)
 	$Snowman/AnimatedSprite.play("running")
 	
 func _process(delta):
 	if continue_scrolling:
-		$ParallaxBackground.set_scroll_offset($ParallaxBackground.scroll_offset + PARALLAX_BACKGROUND_SCROLL_SPEED)
-		$ParallaxForeground.set_scroll_offset($ParallaxForeground.scroll_offset + PARALLAX_FOREGROUND_SCROLL_SPEED)
+		$Background/ParallaxBackground.set_scroll_offset($Background/ParallaxBackground.scroll_offset + PARALLAX_BACKGROUND_SCROLL_SPEED)
+		$Background/ParallaxForeground.set_scroll_offset($Background/ParallaxForeground.scroll_offset + PARALLAX_FOREGROUND_SCROLL_SPEED)
 	
 func _start_level():
 	$LevelDurationTimer.set_wait_time(level_duration)
