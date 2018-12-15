375e4b279ceb7737222ea186f45efd8ff81bfb3c
Zach777 <themanofthehour7778@gmail.com>
2018-12-14 19:52:01 -0500

    Bug fixed for Outside

--- actors/camera/CameraCustom.gd
+++ actors/camera/CameraCustom.gd
@@ -1,7 +1,16 @@
 extends Camera2D
 
 
-
 func _process(delta):
 	if current == true :
 		Pause.position = self.get_camera_screen_center()
+	
+	limit_bottom = 10000
+	limit_right = 10000
+
+
+
+func _ready():
+	limit_bottom = 10000
+	limit_right = 10000
+	
