369a1f05060f3fd00038643647b7e12916b39960
CorvatoCafeto <emma.fs92@gmail.com>
2018-12-15 17:37:50 -0300

    ChimneyDecent4 done

--- scenes/levels/Door.gd
+++ scenes/levels/Door.gd
@@ -7,20 +7,20 @@ onready var t = get_node("Timer")
 func _on_Door_area_entered(area):
 	if(!already):
 		in_area = true
-		$Button.visible = true
+		$Button_Sprite.visible = true
 
 
 func _on_Door_area_exited(area):
 	if(!already):
 		in_area = false
-		$Button.visible = false
+		$Button_Sprite.visible = false
 		
 func _process(delta):
 	if (!already)&&(Input.is_action_just_pressed("ConfirmButton")&&(in_area)):
 		already = true
-		$Door/DoorSFX.play()
-		$Door.play()
+		$Animated_Door_Sprite/DoorSFX.play()
+		$Animated_Door_Sprite.play()
 		t.set_wait_time(1)
 		t.start()
 		yield(t, "timeout")
-		get_node("/root/SceneBrowser").load_scene("CarlsEnding")
+		get_node("/root/SceneBrowser").load_scene("LivingRoom")
