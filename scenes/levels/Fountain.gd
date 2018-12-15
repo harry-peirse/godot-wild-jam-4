8d270db96841e5a57839569e891f052795ea399f
Zach777 <themanofthehour7778@gmail.com>
2018-12-15 08:37:41 -0500

    Fixed Fountain

--- scenes/levels/Fountain.gd
+++ scenes/levels/Fountain.gd
@@ -1,16 +1,26 @@
 extends Area2D
 
-onready var t = get_node("Timer")
+
+
+var health_to_give = 20
 
 signal fountain_entered
 
 func _on_Fountain_area_entered(actor):
+	#Do not do anything if there is no
+	#health to give
+	
 	emit_signal("fountain_entered")
 	$Fountain/FountainDialogue.visible = true
-	t.set_wait_time(4)
-	t.start()
-	yield(t, "timeout")
+#	t.set_wait_time(4)
+#	t.start()
+#	yield(t, "timeout")
 	$Fountain/FountainDialogue.visible = false
+	
+	#Heal the Snowman.
+	actor.get_parent().gain_health( health_to_give )
+	health_to_give = 0
+	self.disconnect( "area_entered", self, "_on_Fountain_area_entered" )
 
 func _on_Fountain_area_exited(actor):
 	$Fountain/FountainDialogue.visible = false
