794e07a72b3b16a700e94f1a86024eb28d9c8227
Zach777 <themanofthehour7778@gmail.com>
2018-12-15 23:02:16 -0500

    Update OutsideDungeon1.gd

--- scenes/levels/OutsideDungeon1.gd
+++ scenes/levels/OutsideDungeon1.gd
@@ -4,5 +4,5 @@ func _ready():
 	Music.play_track(Music.tracks.outside)
 	if LevelManager.get_snowman_location() != null :
 		$Snowman.global_position = LevelManager.get_snowman_location()
-		$Snowman.global_position.y += 200
+		$Snowman.global_position.y += 150
 
