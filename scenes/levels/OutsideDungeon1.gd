483201979a4327608f39be703dce65f60338d621
Zach777 <themanofthehour7778@gmail.com>
2018-12-15 19:58:09 -0500

    Fixed the test I left in OutsideDungeon1.gd

--- scenes/levels/OutsideDungeon1.gd
+++ scenes/levels/OutsideDungeon1.gd
@@ -1,9 +1,6 @@
 extends Node
 
 func _ready():
-	SceneBrowser.load_scene("KrampusFight")
-#	Transition.fade_in()
-	
 	Music.play_track(Music.tracks.outside)
 	if LevelManager.get_snowman_location() != null :
 		$Snowman.global_position = LevelManager.get_snowman_location()
